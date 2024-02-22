6d43c5f8dbead54d4d89b22c3ddc05c240c43735
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-02-22 22:30:44 +0530

    Add Files

---
+++ script.js
@@ -0,0 +1,59 @@
+let msg = document.querySelector("#msg")
+let userScorePara = document.querySelector("#user-score")
+let computerScorePara = document.querySelector("#computer-score")
+
+let userScore = 0;
+let computerScore = 0;
+
+let choices = document.querySelectorAll(".option");
+choices.forEach((choice)=>{
+  choice.addEventListener("click",()=>{
+  const userChoice = choice.getAttribute("id");
+  playGame(userChoice);
+});
+});
+
+const playGame = (userChoice)=>{
+  console.log("users choice is",userChoice);
+  const compChoice = getCompChoice();
+  console.log("comp choice is",compChoice);
+  if (userChoice === compChoice){
+    drawGame();
+  } 
+  else {
+    let userWin = true;
+    if(userChoice === "rock"){ //paper& scissors
+     userWin = compChoice === "paper" ? false:true;
+
+    } else if (userChoice === "paper"){//rock &scissors
+      userWin = compChoice === "scissors"? false:true;
+    }else {//scissors only for user left to choose
+      //comp can have :- rock / paper
+     userWin = compChoice === "rock"?false:true;
+    }
+    showWinner(userWin,userChoice,compChoice); //IDK??????
+  }
+}
+
+const getCompChoice =()=>{
+  const options = ["rock","paper","scissors"];
+  const randomI = Math.floor(Math.random()*3);
+  return options[randomI];
+}
+const drawGame=()=>{
+  msg.innerText= "This Match Was Draw!";
+  msg.style.backgroundColor = "black";
+}
+const showWinner=(userWin,userChoice,compChoice)=>{
+  if (userWin){
+    userScore++
+    userScorePara.innerText = userScore;
+    msg.innerText = `You Win! your ${userChoice} beats ${compChoice} `;
+    msg.style.backgroundColor = "green";
+  }else{
+    computerScore++
+    computerScorePara.innerText = computerScore;
+    msg.innerText = `You Lost! ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+}
